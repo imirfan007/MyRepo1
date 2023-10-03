c1e733206063eeabb5b3b06e27bdbd83a0d3f3f6
irfan <mdirfan96@gmail.com>
2023-10-04 03:35:08 +0530

    ThirdCommit

---
+++ src/myPackage/InfoB.java
@@ -0,0 +1,12 @@
+package myPackage;
+
+public class InfoB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I Am Class InfoB");
+
+	}
+
+}
