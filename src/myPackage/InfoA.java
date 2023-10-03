dbe6be925db6028683b6fe0131c9593958754430
irfan <mdirfan96@gmail.com>
2023-10-04 03:04:10 +0530

    first Commit

---
+++ src/myPackage/InfoA.java
@@ -0,0 +1,13 @@
+package myPackage;
+
+public class InfoA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hi");
+		System.out.println("Hi2");
+		
+	}
+
+}
