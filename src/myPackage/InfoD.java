4b4e1c44d646073a304b99b67911ff1e5c731ad7
irfan <mdirfan96@gmail.com>
2023-10-04 06:52:27 +0530

    from Test1

---
+++ src/myPackage/InfoD.java
@@ -0,0 +1,11 @@
+package myPackage;
+
+public class InfoD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" I am from Class D");
+	}
+
+}
